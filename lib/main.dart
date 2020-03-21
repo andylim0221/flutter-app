91d64ded3f51130e6c79848970807db701280bc1
andylim0221 <limchinhuat0221@gmail.com>
2020-03-21 15:36:24 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home:Text('hey ninja!'),
+  home:Scaffold(
+  appBar: AppBar(
+    title:Text('My first app'),
+    centerTitle: true,
+    backgroundColor: Colors.cyanAccent,
+  ),
+    body: Center(
+        child:Text('Hello world',
+        style: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),),
+  ),
+   floatingActionButton: FloatingActionButton(
+     child:Text('Click'),
+     onPressed: (){},
+   ),
+  ),
 ));
