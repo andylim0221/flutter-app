dd889d3f27e4a481ea3cd73e31c416751d0a8ded
andylim0221 <limchinhuat0221@gmail.com>
2020-03-21 16:17:51 +0800

    Learn buttons, styles, icon, stateless widget, images

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home:Scaffold(
-  appBar: AppBar(
-    title:Text('My first app'),
-    centerTitle: true,
-    backgroundColor: Colors.cyanAccent,
-  ),
-    body: Center(
-        child:Text('Hello world',
-        style: TextStyle(
-          fontSize: 18,
-          fontWeight: FontWeight.bold,
-        ),),
-  ),
-   floatingActionButton: FloatingActionButton(
-     child:Text('Click'),
-     onPressed: (){},
-   ),
-  ),
+  home:Test()
 ));
+
+class Test extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:Text('My first app',style: TextStyle(color: Colors.white),),
+        centerTitle: true,
+        backgroundColor: Colors.purple,
+      ),
+      body: Center(
+        child: RaisedButton.icon(
+          onPressed: (){print('Hello');},
+          icon:Icon(
+            Icons.add_call,
+            color: Colors.white,
+          ),
+          label:Text('Add call',style: TextStyle(color: Colors.white)),
+          color: Colors.purple,
+        )
+        ),
+      floatingActionButton: FloatingActionButton(
+        child:Text('Click'),
+        onPressed: (){print('Hello');},
+      ),
+    );
+  }
+}
